db9afdcf006b3bc5ce40f39ea727980d26a5aa59
JosephMA007 <josephma007@gmail.com>
2015-01-29 19:24:22 -0500

    Fix Merge Bugs
    In NPCWindow form

--- RPG Adventure/NPCWindow.cs
+++ RPG Adventure/NPCWindow.cs
@@ -15,8 +15,6 @@ namespace RPG_Adventure
         private List<Item> inventory;
         private NPC npc;
         private Player player;
-        private System.Windows.Forms.TextBox messageBox;
-        public NPCWindow(NPC npc, Player player, System.Windows.Forms.TextBox messageBox);
         private TextBox messageBox;
         private int lvlmod;
         private Quest quest = new Quest("", "", 0, "", 0, 0, 0);
